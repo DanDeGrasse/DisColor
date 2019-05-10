91c990da467637d7d9607f1a8211f4525cba4d7d
DanDeGrasse <menconidaniel@gmail.com>
2019-05-10 22:42:20 +0200

    Commit I don't know

--- lib/main.dart
+++ lib/main.dart
@@ -185,266 +185,246 @@ class Colore extends State<App> {
         case 0:
           return Center(
             child: Column(children: <Widget>[
-              SizedBox(height: 40),
-              Text("Just Discover",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 46.0,
-                      fontFamily: "Calibre-Semibold",
-                      letterSpacing: 1.0)),
-              SizedBox(height: 10),
+              SizedBox(height: 65),
+              Text(
+                "A New Way To Discover.",
+                style: TextStyle(
+                    fontFamily: "OpenSans-Regular",
+                    color: Color(0xff263238),
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Just New Colors.",
+                style: TextStyle(
+                    fontFamily: "OpenSans-Regular",
+                    color: Color(0xff263238),
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 75),
               AnimatedContainer(
                 decoration: BoxDecoration(
-                  color: colors[indexcolor],
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                height: 405,
-                width: 374,
+                    color: colors[indexcolor],
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 1,
+                      ),
+                    ]),
+                height: 200,
+                width: 300,
                 duration: Duration(seconds: 1),
                 child: Center(
                   child: Text(coloreTesto,
                       style: TextStyle(
-                          fontSize: 40.0, fontFamily: "Calibre-Semibold")),
+                          fontSize: 30.0, fontFamily: "OpenSans-Regular")),
                 ),
               ),
-              SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "As Simple As You Think",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
+              SizedBox(height: 80),
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Open Application That Helps",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      Text(
+                        "Colorblind People. Just Enjoy",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                    ],
                   ),
                 ),
               )
             ]),
           );
         case 1:
-          return PageView(
-            children: <Widget>[
-              Column(
+          return Column(children: <Widget>[
+            SizedBox(height: 70),
+            Expanded(
+              child: PageView(
                 children: <Widget>[
-                  SizedBox(width: 100, height: 45),
-                  Text(
-                    "The Red Color",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: "Calibre-Semibold"),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(width: 100, height: 45),
+                      Text(
+                        "The Red Color",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      SizedBox(height: 50),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 400,
+                        width: 300,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
                   ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 400,
-                    width: 300,
+                  Column(
+                    children: <Widget>[
+                      SizedBox(width: 100, height: 45),
+                      Text(
+                        "The Blue Color",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      SizedBox(height: 50),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 400,
+                        width: 300,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 35,
+                  Column(
+                    children: <Widget>[
+                      SizedBox(width: 100, height: 45),
+                      Text(
+                        "The Green Color",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      SizedBox(height: 50),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 400,
+                        width: 300,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
                   ),
-                  Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "Just Tap The Color",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(width: 100, height: 45),
+                      Text(
+                        "The Yellow Color",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      SizedBox(height: 50),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 400,
+                        width: 300,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
                   ),
-                ),
-              ),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(width: 100, height: 45),
+                      Text(
+                        "The Purple Color",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                            fontFamily: "OpenSans-Regular"),
+                      ),
+                      SizedBox(height: 50),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.purple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        height: 400,
+                        width: 300,
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
+                  ),
                 ],
               ),
-              Column(
-                children: <Widget>[
-                  SizedBox(width: 100, height: 45),
-                  Text(
-                    "The Blue Color",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: "Calibre-Semibold"),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 400,
-                    width: 300,
-                  ),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "Just Tap The Color",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
-                  ),
-                ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
               ),
-                ],
+              child: Text(
+                "A New Way To Learn",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontFamily: "OpenSans-Regular"),
               ),
-              Column(
-                children: <Widget>[
-                  SizedBox(width: 100, height: 45),
-                  Text(
-                    "The Green Color",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: "Calibre-Semibold"),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 400,
-                    width: 300,
-                  ),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "Just Tap The Color",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
-                  ),
-                ),
-              ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  SizedBox(width: 100, height: 45),
-                  Text(
-                    "The Yellow Color",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: "Calibre-Semibold"),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 400,
-                    width: 300,
-                  ),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "Just Tap The Color",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
-                  ),
-                ),
-              ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  SizedBox(width: 100, height: 45),
-                  Text(
-                    "The Purple Color",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontFamily: "Calibre-Semibold"),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.purple,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    height: 400,
-                    width: 300,
-                  ),
-                  SizedBox(
-                    height: 35,
-                  ),
-                  Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                width: 280,
-                height: 50,
-                child: Center(
-                  child: Text(
-                    "Just Tap The Color",
-                    style:
-                        TextStyle(fontFamily: "Calibre-Semibold", fontSize: 18),
-                  ),
-                ),
-              ),
-                ],
-              ),
-            ],
-          );
+            ),
+            SizedBox(height: 80),
+          ]);
         case 2:
           return Column(
             children: <Widget>[
-
               SizedBox(height: 50),
               Center(
                 child: Column(children: <Widget>[
                   Text("Daniel Menconi",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: "Calibre-Semibold",
+                          color: Colors.black,
+                          fontFamily: "OpenSans-Regular",
                           fontSize: 24.0)),
-                          SizedBox(height: 25),
+                  SizedBox(height: 25),
                   Text(
-                    "A young 16 years old guy from Itay with the passion",
-                    style: TextStyle(color: Colors.white),
+                    "16 years old guy from Itay with the passion",
+                    style: TextStyle(color: Colors.black),
                   ),
                   Text(
-                    "of programming studyng ICT at School ",style: TextStyle(color: Colors.white),
+                    "of programming studyng ICT at School ",
+                    style: TextStyle(color: Colors.black),
                   ),
                   Text(
-                    "I'm learning the Dart language & C language",style: TextStyle(color: Colors.white),
+                    "I'm learning the Dart language & C language",
+                    style: TextStyle(color: Colors.black),
                   ),
                   Text(
-                    "I know something of JavaScript and Html. Enjoy my app!",style: TextStyle(color: Colors.white),
+                    "I know something of JavaScript and Html. Enjoy my app!",
+                    style: TextStyle(color: Colors.black),
                   ),
                 ]),
               )
@@ -466,13 +446,11 @@ class Colore extends State<App> {
           },
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-        backgroundColor: Color(0xFF2d3447),
+        backgroundColor: Colors.white,
         body: Container(child: selectedCol(context, _indextap)),
         bottomNavigationBar: BubbleBottomBar(
           fabLocation: BubbleBottomBarFabLocation.end,
           currentIndex: _indextap,
-          hasNotch: true,
-          hasInk: true,
           inkColor: Colors.black12,
           opacity: .2,
           onTap: (index) {
